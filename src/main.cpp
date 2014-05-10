0d6595d39008233057649ef7df244651b3441561
Laura Estefanía Corvalán <corvalan.laura@gmail.com>
2014-05-10 05:58:47 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,8 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
    string nombreArchivo;
-   nombreArchivo = "/home/laurac/src/fiuba/tp1/pepe.txt";
-   // cout << "Ingrese el path del archivo: ";
-   // cin >> nombreArchivo;
+   cout << "Ingrese el path del archivo: ";
+   cin >> nombreArchivo;
 
    Parser parser(nombreArchivo);
    parser.iniciar();
@@ -21,8 +20,6 @@ int main(int argc, char *argv[]) {
    Laberinto laberinto;
    laberinto.generarRecorridosDesdeListaDeComandos(listaComandos);
 
-   // Se iteran los recorridos del laberinto y se imprime en pantalla
-   // la info de cada casillero
    Mochila * mochila = laberinto.obtenerMochila();
    cout << "Mochila: " << endl;
    mochila->mostrar();
