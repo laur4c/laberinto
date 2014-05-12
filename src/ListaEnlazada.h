9bf5de6657d37adb6c11af10cb17d80aa0687790
Laura Estefania Corvalan <corvalan.laura@gmail.com>
2014-05-12 15:45:47 -0300

    Updates Lista.h

--- src/ListaEnlazada.h
+++ src/ListaEnlazada.h
@@ -20,10 +20,18 @@ template<class T> class ListaEnlazada {
        */
       ListaEnlazada();
 
+      bool estaVacia();
+
       /**
        * Agrega un elemento al final de la lista.
        */
       void agregar(T * dato, int posicion);
+
+
+      /*
+      * Agrega el elemento al final de la Lista
+      */
+      void agregar(T * dato);
 
       /**
        * Remueve un elementode la lista.
@@ -36,7 +44,24 @@ template<class T> class ListaEnlazada {
 
       void mostrar();
 
-      // ~ListaEnlazada();
+      /*
+       * Deja el cursor de la Lista preparado para hacer un nuevo
+       * recorrido sobre sus elementos.
+       */
+      void iniciarCursor();
+
+      /*
+       * Mueve el cursor y lo posiciona en el siguiente elemento
+       */
+      bool avanzarCursor();
+
+      /**
+       * El cursor está posicionado sobre un elemento de la Lista,
+       * Devuelve el elemento en la posición del cursor.
+       */
+      T * obtenerCursor();
+
+      ~ListaEnlazada();
    private:
 
       /**
@@ -45,6 +70,10 @@ template<class T> class ListaEnlazada {
       Nodo<T> * frente;
 
       int tamanio;
+
+      Nodo<T> * cursor;
+
+      Nodo<T> * obtenerNodo(int posicion);
 };
 
 
@@ -52,48 +81,102 @@ template<class T>
 ListaEnlazada<T>::ListaEnlazada() {
    this->frente = NULL;
    this->tamanio = 0;
+   this->cursor = NULL;
+}
+
+template<class T>
+bool ListaEnlazada<T>::estaVacia() {
+   return this->tamanio == 0;
 }
 
 template<class T>
 void ListaEnlazada<T>::agregar(T * dato, int posicion) {
-   Nodo<T> * nodo = new Nodo<T>(dato);
-   if (posicion == 1) {
-      nodo->cambiarSiguiente(nodo);
-      this->frente = nodo;
-   } else {
-      Nodo<T> * unNodo = this->frente;
-      for (int i= 1; i < (posicion - 1) && unNodo && unNodo != NULL; i++) {
-         unNodo = unNodo->obtenerSiguiente();
+   if ((posicion > 0) && (posicion <= this->tamanio + 1)) {
+      Nodo<T> * nodo = new Nodo<T>(dato);
+      if (posicion == 1) {
+         nodo->cambiarSiguiente(this->frente);
+         this->frente = nodo;
+      } else {
+         Nodo<T> * anterior = this->obtenerNodo(posicion - 1);
+         nodo->cambiarSiguiente(anterior->obtenerSiguiente());
+         anterior->cambiarSiguiente(nodo);
       }
-      nodo->cambiarSiguiente(unNodo->obtenerSiguiente());
-      unNodo->cambiarSiguiente(nodo);
+      this->tamanio++;
+      this->iniciarCursor();
    }
-   this->tamanio++;
+}
+
+template<class T>
+void ListaEnlazada<T>::agregar(T * dato) {
+   this->agregar(dato, this->tamanio + 1);
 }
 
 template<class T>
 T * ListaEnlazada<T>::obtenerElemento(int posicion) {
-   // @TODO mejorar con cursor
    T * elemento;
-   if (posicion > 0 && posicion <= this->tamanio) {
-      Nodo<T> * actual = this->frente;
-      for (int i = 1; i < posicion; i++) {
-         actual = actual->obtenerSiguiente();
-      }
-      elemento = actual->obtenerDato();
+   if ((posicion > 0) && (posicion <= this->tamanio)) {
+      elemento = this->obtenerNodo(posicion)->obtenerDato();
    }
    return elemento;
 }
 
 template<class T>
 T * ListaEnlazada<T>::eliminar(int posicion) {
-   // @TODO: falta implementar
-   return this->obtenerElemento(posicion);
+   if ((posicion > 0) && (posicion <= this->tamanio)) {
+      Nodo<T>* removido;
+
+      if (posicion == 1) {
+         removido = this->frente;
+         this->frente = removido->obtenerSiguiente();
+      } else {
+         Nodo<T> * anterior = this->obtenerNodo(posicion - 1);
+         removido = anterior->obtenerSiguiente();
+         anterior->cambiarSiguiente(removido->obtenerSiguiente());
+      }
+
+      delete removido;
+      this->tamanio--;
+
+      this->iniciarCursor();
+   }
 }
 
 template<class T>
 int ListaEnlazada<T>::obtenerTamanio() {
    return this->tamanio;
+}
+
+template<class T>
+void ListaEnlazada<T>::iniciarCursor() {
+   this->cursor = NULL;
+}
+
+template<class T>
+bool ListaEnlazada<T>::avanzarCursor() {
+   if (this->cursor == NULL) {
+      this->cursor = this->frente;
+   } else {
+      this->cursor = this->cursor->obtenerSiguiente();
+   }
+   return this->cursor != NULL;
+}
+
+template<class T>
+T * ListaEnlazada<T>::obtenerCursor() {
+   T * elemento;
+   if (this->cursor != NULL) {
+      elemento = this->cursor->obtenerDato();
+   }
+   return elemento;
+}
+
+template<class T>
+Nodo<T> * ListaEnlazada<T>::obtenerNodo(int posicion) {
+   Nodo<T> * actual = this->frente;
+   for (int i = 1; i < posicion; i++) {
+      actual = actual->obtenerSiguiente();
+   }
+   return actual;
 }
 
 template<class T>
@@ -105,12 +188,14 @@ void ListaEnlazada<T>::mostrar() {
    }
 }
 
-// template<class T>
-// ListaEnlazada::~ListaEnlazada() {
-//    while(this->tamanio > 0) {
-//       this->eliminar(1);
-//    }
-//    delete this->frente;
-// }
+template<class T>
+ListaEnlazada<T>::~ListaEnlazada() {
+   while (this->frente != NULL) {
+      Nodo<T> * aBorrar = this->frente;
+      this->frente = this->frente->obtenerSiguiente();
+
+      delete aBorrar;
+   }
+}
 
 #endif
