2101d63c2f0b24ad59ba97d3c2bb94c1cea9a5bb
Laura Estefania Corvalan <corvalan.laura@gmail.com>
2014-05-25 17:13:08 -0300

    bug fixes

--- src/main.cpp
+++ src/main.cpp
@@ -11,34 +11,39 @@ int main(int argc, char *argv[]) {
    string nombreArchivo;
    cout << "Ingrese el path del archivo: ";
 
-   cin >> nombreArchivo;
-   Parser parser(nombreArchivo);
+   try {
+      cin >> nombreArchivo;
+      Parser parser(nombreArchivo);
 
-   // Se genera una lista con comandos. Cada comando representa una directiva
-   // del archivo de texto
-   parser.iniciar();
-   Cola<Comando*> * listaComandos = parser.obtenerLista();
-   // cout << "Lista de comandos" << endl;
-   // listaComandos->mostrar();
+      // Se genera una lista con comandos. Cada comando representa una directiva
+      // del archivo de texto
+      parser.iniciar();
+      Cola<Comando*> * listaComandos = parser.obtenerLista();
+      // cout << "Lista de comandos" << endl;
+      // listaComandos->mostrar();
 
-   // A partir de la lista de comandos, se crea una lista de caminos
-   // El camino es una estructura de puntos entrelazados (cada punto tiene
-   // cuatro punteros, que representan las uniones del punto
-   // con otros puntos en todas las direcciones: norte, sur, este, oeste)
-   // Estos puntos pretenden representar todos los recorridos del laberinto
-   Laberinto * laberinto = new Laberinto();
-   laberinto->crearCaminosDesdeListaDeComandos(listaComandos);
+      // A partir de la lista de comandos, se crea una lista de caminos
+      // El camino es una estructura de puntos entrelazados (cada punto tiene
+      // cuatro punteros, que representan las uniones del punto
+      // con otros puntos en todas las direcciones: norte, sur, este, oeste)
+      // Estos puntos pretenden representar todos los recorridos del laberinto
+      Laberinto * laberinto = new Laberinto();
+      laberinto->crearCaminosDesdeListaDeComandos(listaComandos);
 
-   Mochila * mochila = laberinto->obtenerMochila();
-   cout << "Estado de la mochila del caminante: " << endl;
-   mochila->mostrar();
+      Mochila * mochila = laberinto->obtenerMochila();
+      cout << "Estado de la mochila del caminante: " << endl;
+      mochila->mostrar();
 
-   cout << "Description cuantitativa del recorrido realizado por el caminante: " << endl;
-   laberinto->mostrarInfo();
+      cout << "Description cuantitativa del recorrido realizado por el caminante: " << endl;
+      laberinto->mostrarInfo();
 
-   ImagenLaberinto * imagen = new ImagenLaberinto(laberinto->obtenerCaminos());
-   imagen->generar();
+      ImagenLaberinto * imagen = new ImagenLaberinto(laberinto->obtenerCaminos());
+      imagen->generar();
 
-   delete laberinto;
+      delete laberinto;
+   } catch (const char* msg) {
+      cout << msg << endl;
+   }
+
    return 0;
 }
